b972ad8d3634f40893cf8417cf430344b1d716d5
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-12-19 15:29:03 +0530

    Adding Navigating Programmatically

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,7 +1,27 @@
+import { useRouter } from "next/router";
+
+//Navigating Programmatically
 function PorfolioPage() {
+  const router = useRouter();
+
+  function loadProjectHandlerProject() {
+    // router.push("portfolio/project123");
+    //router.replace(portfolio/project123)
+    router.push({
+      pathname: "portfolio/[projectId]",
+      query: { projectId: "project123" },
+    });
+  }
+
   return (
     <div>
       <h2> This is Portfolio Page</h2>
+      <button
+        className=" bg-stone-500 hover:bg-stone-400"
+        onClick={loadProjectHandlerProject}
+      >
+        Load the Portfolio
+      </button>
     </div>
   );
 }
